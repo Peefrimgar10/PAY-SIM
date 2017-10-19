5724de038472b80c6c1d4ecd1ba1960852fe9809
ryanwand <32582391+ryanwand@users.noreply.github.com>
2017-10-18 21:34:26 -0400

    Update payloadModel.m

--- payloadModel.m
+++ payloadModel.m
@@ -2,6 +2,7 @@ function payloadModel(wavelengthTop, wavelengthBottom, binSize, apparentMagnitud
 format long
 %% Read Empirical Solar Data
 [TrueIrradiance] = SolarRead();
+MY DUDE EMANUEL
 %% Create Proportioned Black Body Spectrum:
 %[proportionedWavelengths,Lam] = UnitBlackBody(binSize, T);
 
